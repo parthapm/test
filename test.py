9d73a50252ec697044611349de064a1288bbab90
parthapm <53114646+parthapm@users.noreply.github.com>
2019-07-20 16:50:33 +0530

    Added test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+password = ''
+while password != 'partha123':
+      password = input("Enter Password:")
+      if password == 'partha123':
+          print ("Welcome to our site")
+      else:
+          print('Sorry, Try Again')
